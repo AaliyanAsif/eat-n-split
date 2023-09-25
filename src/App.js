26aac362b14151e1044cc3e6f8f9bacf3e12d826
AaliyanAsif <aaliyan876@gmail.com>
2023-09-25 07:05:08 +0500

    UI + addFormFriend updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
-
+import { useState } from "react";
+import Button from "./components/Button";
+import FormAddFriend from "./components/FormAddFriend";
+import FormSplitBill from "./components/FormSplitBill";
+import FriendsList from "./components/FriendsList";
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
   return (
-    <div className="App">
-  
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
+      </div>
+
+      <FormSplitBill />
     </div>
   );
 }
---
+++ src/components/FormAddFriend.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "./Button";
+
+export default function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>Friend name 👦</label>
+      <input type="text" />
+
+      <label>Image URL 📷</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
